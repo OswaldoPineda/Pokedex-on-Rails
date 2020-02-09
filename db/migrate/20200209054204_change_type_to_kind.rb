c472c9b9e562d813906744c02fda52b344597d4a
OswaldoPineda <carlos-1502010@hotmail.com>
2020-02-08 23:52:09 -0600

    Change the Type field name to Kind

---
+++ db/migrate/20200209054204_change_type_to_kind.rb
@@ -0,0 +1,5 @@
+class ChangeTypeToKind < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :pokemons, :type, :kind
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_053615) do
+ActiveRecord::Schema.define(version: 2020_02_09_054204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "kind"
     t.integer "generation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
