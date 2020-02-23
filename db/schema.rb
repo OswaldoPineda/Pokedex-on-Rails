e16405495057329e18ef7e24c6c574888e2b5434
OswaldoPineda <carlos-1502010@hotmail.com>
2020-02-23 00:23:11 -0600

    Create te Kind model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_054204) do
+ActiveRecord::Schema.define(version: 2020_02_23_060639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "kinds", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
